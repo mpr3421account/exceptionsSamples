75d3d209df3c9592fd6f83313617a503f28381ff
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2023-01-09 12:50:01 -0300

    Try-Catch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+try
+{
+    int n1 = int.Parse(Console.ReadLine());
+    int n2 = int.Parse(Console.ReadLine());
+
+    int result = n1 / n2;
+    Console.WriteLine(result);
+}
+catch(DivideByZeroException e)
+{
+    Console.WriteLine("Error!" + e.Message);
+}
