810ad9e4cc107e7e01003c0f770c02c09b8f6d6d
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2023-01-10 08:37:12 -0300

    Bad Solution.

--- Entities/Reservation.cs
+++ Entities/Reservation.cs
@@ -9,33 +9,44 @@ namespace exceptionsSamples.Entities
     internal class Reservation
     {
         public int RoomNumber { get; set; }
-        public DateTime Checkin { get; set; }
-        public DateTime Checkout { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
 
-        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
         {
             RoomNumber = roomNumber;
-            Checkin = checkin;
-            Checkout = checkout;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
         }
         public double Duration()
         {
-            TimeSpan duration = Checkout.Subtract(Checkin);
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
             return(int)duration.TotalDays;
         }
-        public void updateDates(DateTime checkin, DateTime checkout)
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
         {
-            Checkin = checkin;
-            Checkout = checkout;
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return "Error in reservation: Reservation dates for update must be future dates";
+            }
+            if (checkOut <= checkIn)
+            {
+                return "Error in reservation: Check-out date must be after check-in date";
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            return null;
         }
         public override string ToString()
         {
             return "Room:" 
                 + RoomNumber
                 + ", checkin: "
-                + Checkin.ToString("dd/MM/yyyy")
+                + CheckIn.ToString("dd/MM/yyyy")
                 + ", checkout: "
-                + Checkout.ToString("dd/MM/yyyy")
+                + CheckOut.ToString("dd/MM/yyyy")
                 + ", "
                 + Duration()
                 + " nights";
